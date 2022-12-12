cd7f0edd61ef4f3804d8e3b47d3a674a0e381630
Mike Muscato <Michael.Muscato@t-mobile.com>
2022-12-12 11:07:12 -0700

    Styles and linting/spacing updates.

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname+'/dist/d-team-frontend'));
+app.use(express.static(__dirname + '/dist/d-team-frontend'));
 
 app.get('/*', (req, resp) => {
-    resp.sendFile(__dirname+'/dist/d-team-frontend/index.html');
+    resp.sendFile(__dirname + '/dist/d-team-frontend/index.html');
 });
 
 app.listen(process.env.PORT || 8081);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'D-Team Academics - School Management System';
+    title = 'D-Team Academics - School Management System';
 }
