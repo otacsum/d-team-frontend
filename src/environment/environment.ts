0f48090f17a3f908c9c042ff56950a09149ab2ea
Mike Muscato <Michael.Muscato@t-mobile.com>
2022-12-15 16:06:48 -0700

    Reset api to remote version

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const Environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:3000',
-    //apiBaseUrl: 'https://d-team-backend.herokuapp.com',
+    //apiBaseUrl: 'http://localhost:3000',
+    apiBaseUrl: 'https://d-team-backend.herokuapp.com',
 };
